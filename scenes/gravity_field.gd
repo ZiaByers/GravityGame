e854b330d4e43c831ccb53e1d5d0184a9fd96291
ZiaByers <ziabyers@gmail.com>
2024-02-02 18:06:12 -0700

    deleted print statements

--- scenes/gravity_field.gd
+++ scenes/gravity_field.gd
@@ -4,7 +4,3 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gravity_space_override = 4
-	print(gravity)
-	
-func _process(_delta):
-	print(gravity)
